4237dba0d74b06c2ac649058fff190e2b0914236
Justin Berli <justinberli@gmail.com>
2025-03-26 15:36:43 +0100

    updated for deployment

--- server/index.js
+++ server/index.js
@@ -6,10 +6,10 @@ const file = fs.readFileSync('./server/configuration.yml', { encoding: 'utf-8' }
 const params = load(file);
 
 const app = express()
-const port = 8001
+const port = 8002
 const __dirname = import.meta.dirname;
 
-app.use('/cartogame/configuration', (req, res) => {
+app.use('/cartogame-builder/configuration', (req, res) => {
 	res.json(params);
 	return res;
 });
